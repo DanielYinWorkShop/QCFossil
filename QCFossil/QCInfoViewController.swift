9f7bb9b9834a42d950ce5ca428d3214a39cebf27
Daniel <yamado.kira@gmail.com>
2019-07-15 23:00:58 +0800

    Add QCInfo page

---
+++ QCFossil/QCInfoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  QCInfoViewController.swift
+//  QCFossil
+//
+//  Created by pacmobile on 15/7/2019.
+//  Copyright © 2019 kira. All rights reserved.
+//
+
+import Foundation
+
+class QCInfoViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+    }
+    
+    
+}
